bdfd3daaf636d58aa1e45089916e7194c9eef787
Jacob Hummer <jcbhmr@outlook.com>
2023-08-01 15:12:35 -0500

    more idea

--- index.js
+++
@@ -1,2 +0,0 @@
-export { default as Ubuntu2004AMD64Controller } from "./ubuntu-20.04-amd64/index.js";
-export { default as Ubuntu2204AMD64Controller } from "./ubuntu-22.04-amd64/index.js";
---
+++ src/index.js
@@ -0,0 +1,2 @@
+export { default as Ubuntu2004AMD64Controller } from "./ubuntu-20.04-amd64.js";
+export { default as Ubuntu2204AMD64Controller } from "./ubuntu-22.04-amd64.js";
